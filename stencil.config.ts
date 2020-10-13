2ea671858e228eba8c9943f1a3fd2c5e696dcfd7
Todsaporn Sangpook <todsaporn.s@geotalent.co.th>
2020-10-13 08:41:58 +0700

    update config

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,21 +2,22 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'ice-component-react',
-  bundles: [{ components: ['my-component'] }, { components: ['my-button'] }],
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements-bundle',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
-  ],
+  srcDir: 'src',
+  // bundles: [{ components: ['my-component'] }, { components: ['my-button'] }],
+  // outputTargets: [
+  //   {
+  //     type: 'dist',
+  //     esmLoaderPath: '../loader',
+  //   },
+  //   {
+  //     type: 'dist-custom-elements-bundle',
+  //   },
+  //   {
+  //     type: 'docs-readme',
+  //   },
+  //   {
+  //     type: 'www',
+  //     serviceWorker: null, // disable service workers
+  //   },
+  // ],
 };
